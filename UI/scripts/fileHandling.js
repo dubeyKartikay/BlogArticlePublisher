ea1133bd01b907a10d79b7c082e611e6f6065a13
Kartikay Dubey <kartikayd2003@gmail.com>
2023-07-16 23:42:24 +0530

    Ui improvements

---
+++ UI/scripts/fileHandling.js
@@ -0,0 +1,177 @@
+let imageFile = null;
+let imageFileData = null;
+let contentFile = null;
+let blogMarkDown = null;
+// Add event listeners for drag and drop
+const dropAreaImage = document.getElementById("image");
+const dropAreaContent = document.getElementById("content");
+// const imagePreview = document.getElementById("imagePreview");
+// const contentPreview = document.getElementById("contentPreview");
+
+// Prevent default drag behaviors
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  dropAreaImage.addEventListener(eventName, preventDefaults, false);
+  dropAreaContent.addEventListener(eventName, preventDefaults, false);
+});
+
+// Highlight drop area when item is dragged over it
+["dragenter", "dragover"].forEach((eventName) => {
+  dropAreaImage.addEventListener(eventName, highlight, false);
+  dropAreaContent.addEventListener(eventName, highlight, false);
+});
+
+// Remove highlight when item is no longer dragged over drop area
+["dragleave", "drop"].forEach((eventName) => {
+  dropAreaImage.addEventListener(eventName, unhighlight, false);
+  dropAreaContent.addEventListener(eventName, unhighlight, false);
+});
+
+// Handle dropped files
+dropAreaImage.addEventListener("drop", handleDropFile, false);
+dropAreaContent.addEventListener("drop", handleDropContent, false);
+dropAreaImage.addEventListener("click", openFileExplorer.bind(null, "image"));
+dropAreaContent.addEventListener(
+  "click",
+  openFileExplorer.bind(null, "content")
+);
+
+function displayPreviewImage(file, previewElement) {
+  const src = URL.createObjectURL(file);
+  previewElement.style.backgroundImage = `url(${src})`
+  // previewElement.innerText = "";
+  previewElement.children[1].innerText = "";
+  removeErrorMsg(previewElement);
+  previewElement.classList.add("file-selected")
+}
+
+function displayPreviewContent(file, previewElement) {
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    removeErrorMsg(previewElement);
+    previewElement.children[1].innerText = `${e.target.result}`;
+    previewElement.classList.add("file-selected")
+  };
+
+  reader.onerror = function(e){
+    console.log(e);
+    handleErrorFile(previewElement);
+  }
+  reader.readAsText(file);
+}
+function openFileExplorer(elementId) {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = elementId === "image" ? "image/*" : ".md";
+  fileInput.style.display = "none";
+
+  fileInput.addEventListener(
+    "change",
+    handleFileSelection.bind(null, elementId)
+  );
+
+  document.body.appendChild(fileInput);
+  fileInput.click();
+}
+function handleFileSelection(elementId, event) {
+  const fileInput = event.target;
+  const file = fileInput.files[0];
+
+  if (elementId === "image") {
+    handleImageFile(file);
+  } else if (elementId === "content") {
+    handleContentFile(file);
+  }
+
+  fileInput.remove();
+}
+
+function removeErrorMsg(fileDOM){
+  errorDOM = fileDOM.firstElementChild;
+  errorDOM.innerText = "";
+  errorDOM.classList.remove("error-file");
+}
+function handleErrorFile(fileDOM, message="An error occured while handling this file") {
+  errorDOM = fileDOM.firstElementChild;
+  // console.log(errorDOM);
+  // fileDOM.childNodes[1].data = "";
+  errorDOM.classList.add("error-file");
+  errorDOM.innerText = message;
+  // fileDOM.removeChild(fileDOM.children[1])
+  fileDOM.children[1].innerText = ""
+  // fileDOM.removeChild(fileDOM.firstChild)
+  // fileDOM.removeChild(fileDOM.firstChild)
+}
+
+function preventDefaults(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function highlight() {
+  this.classList.add("highlight");
+}
+
+function unhighlight() {
+  this.classList.remove("highlight");
+}
+
+function handleDropFile(event) {
+  const files = event.dataTransfer.files;
+  if (files.length > 0) {
+    handleImageFile(files[0]);
+  }
+}
+
+function handleDropContent(event) {
+  const files = event.dataTransfer.files;
+  if (files.length > 0) {
+    handleContentFile(files[0]);
+  }
+}
+async function handleImageFile(file) {
+  try{
+    imageFileData = await readFile(file);
+  }catch(e){
+    console.log(e);
+    handleErrorFile(dropAreaImage)
+  }
+
+  if (!imageFileData) {
+    handleErrorFile(dropAreaImage);
+    return;
+  }
+
+  imageFile = file;
+  console.log(imageFile);
+  displayPreviewImage(file, dropAreaImage);
+}
+
+async function handleContentFile(file) {
+  blogMarkDown = await readFile(file);
+  if (!blogMarkDown) {
+    handleErrorFile(dropAreaContent);
+    return;
+  }
+
+  contentFile = file;
+  console.log(contentFile);
+  displayPreviewContent(file, dropAreaContent);
+}
+const readFile = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const fileVAl = e.target.result;
+      resolve(fileVAl);
+    };
+    reader.onerror = (err) => {
+      reject(`Error Occurred while reading  ${err}`);
+    };
+
+    if (file.type.includes("image")) {
+      reader.readAsDataURL(file);
+    } else {
+      reader.readAsText(file);
+    }
+  });
+};
--- UI/scripts/renderer.js
+++ UI/scripts/renderer.js
@@ -1,123 +1,18 @@
 // const { ipcRenderer } = window;
 document.getElementById("form").addEventListener("submit", sendForm);
-let imageFile = null;
-let contentFile = null;
-// Add event listeners for drag and drop
-const dropAreaImage = document.getElementById("image");
-const dropAreaContent = document.getElementById("content");
-
-// Prevent default drag behaviors
-["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
-  dropAreaImage.addEventListener(eventName, preventDefaults, false);
-  dropAreaContent.addEventListener(eventName, preventDefaults, false);
-});
-
-// Highlight drop area when item is dragged over it
-["dragenter", "dragover"].forEach((eventName) => {
-  dropAreaImage.addEventListener(eventName, highlight, false);
-  dropAreaContent.addEventListener(eventName, highlight, false);
-});
-
-// Remove highlight when item is no longer dragged over drop area
-["dragleave", "drop"].forEach((eventName) => {
-  dropAreaImage.addEventListener(eventName, unhighlight, false);
-  dropAreaContent.addEventListener(eventName, unhighlight, false);
-});
-
-// Handle dropped files
-dropAreaImage.addEventListener("drop", handleDropImage, false);
-dropAreaContent.addEventListener("drop", handleDropContent, false);
-
-function handleErrorFile() {
-	document.body.appendChild(errorMessage);
-	errorMessage.classList.add("show");
-  
-	// Remove the error message after 3 seconds
-	setTimeout(() => {
-	  errorMessage.classList.remove("show");
-	  setTimeout(() => {
-		errorMessage.remove();
-	  }, 300);
-	}, 3000);
-  }
-
-function preventDefaults(event) {
-  event.preventDefault();
-  event.stopPropagation();
-}
-
-function highlight() {
-  this.classList.add("highlight");
-}
-
-function unhighlight() {
-  this.classList.remove("highlight");
-}
-
-function handleDropImage(event) {
-  const files = event.dataTransfer.files;
-  if (files.length > 0) {
-    handleImageFile(files[0]);
-  }
-}
-
-function handleDropContent(event) {
-  const files = event.dataTransfer.files;
-  if (files.length > 0) {
-    handleContentFile(files[0]);
-  }
-}
-async function handleImageFile(file) {
-  const imageFileData = await readFile(file);
-  if (!imageFileData) {
-    handleErrorFile();
-    return;
-  }
-
-  imageFile = file;
-}
-
-async function handleContentFile(file) {
-  const blogMarkDown = await readFile(file);
-  if (!blogMarkDown) {
-    handleErrorFile();
-    return;
-  }
-
-  contentFile = file;
-}
-const readFile = (file) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const fileVAl = e.target.result;
-      resolve(fileVAl);
-    };
-    reader.onerror = (err) => {
-      reject(`Error Occurred while reading  ${err}`);
-    };
-
-    if (file.type.includes("image")) {
-      reader.readAsDataURL(file);
-    } else {
-      reader.readAsText(file);
-    }
-  });
-};
-const handleErrorFile = () => {
-  console.log("File Error");
-};
-
 async function sendForm(event) {
   event.preventDefault();
-  const imageFileData = await getInputFile("image");
-  const blogMarkDown = await getInputFile("content");
-  if (!imageFileData) {
-    handleErrorFile();
+  // const imageFileData = await getInputFile("image");
+  // const blogMarkDown = await getInputFile("content");
+  if (imageFileData == null) {
+    handleErrorFile(dropAreaImage);
+    if (blogMarkDown == null) {
+      handleErrorFile(dropAreaContent);
+    }
     return;
   }
-  if (!blogMarkDown) {
-    handleErrorFile();
+  if (blogMarkDown == null) {
+    handleErrorFile(dropAreaContent);
     return;
   }
   const { heading, subhead, time } = event.target;
@@ -131,27 +26,4 @@ async function sendForm(event) {
   };
 
   ipcRenderer.send("form-submission", data);
-}
-const getContentPath = () => {
-  const fileInput = document.getElementById("content");
-  const file = fileInput.files[0];
-  return file.path.substring(0, file.path.lastIndexOf("/"));
-};
-async function getInputFile(elementId) {
-  const fileInput = document.getElementById(elementId);
-  const file = fileInput.files[0];
-  const acceptableFileTypes =
-    elementId == "image" ? ["png", "jpg", "jpeg"] : ["md"];
-  if (!acceptableFileTypes.includes(file.path.split(".").pop().toLowerCase())) {
-    //handle wrong file type
-    return null;
-  }
-  let fileData = null;
-  try {
-    fileData = await readFile(file);
-  } catch (err) {
-    console.log(err);
-    fileData = null;
-  }
-  return fileData;
 }
